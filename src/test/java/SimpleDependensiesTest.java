05b4fdb1858ef293a27b365b305aaa43a626af6a
royabanz <royabanz@mts.ru>
2017-04-05 17:24:52 +0700

    Fixed gitignore.

---
+++ src/test/java/SimpleDependensiesTest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by royabanz on 05.04.2017.
+ * Class witch must to realize CRUD operations on test database.
+ * Database contains three entities.
+ *
+ */
+public class SimpleDependensiesTest {
+}
